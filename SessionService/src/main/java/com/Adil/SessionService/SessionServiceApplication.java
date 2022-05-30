b882ed6cefb6a64e9448b1ee3c0a942101576b9b
lpov <adilshk097@gmail.com>
2022-05-30 22:16:58 +0530

    SessionService

---
+++ SessionService/src/main/java/com/Adil/SessionService/SessionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Adil.SessionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionServiceApplication.class, args);
+	}
+
+}
